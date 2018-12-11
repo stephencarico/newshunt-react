a38fa7c216a10432363f5a9d7ecc7e48e1e5002a
Stephen Carico <stephen.r.carico@gmail.com>
2018-12-10 19:15:37 -0800

    initial crash test

--- src/NewsHunt.test.js
+++ src/NewsHunt.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NewsHunt from './NewsHunt';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NewsHunt />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('NewsHunt', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NewsHunt />, div)
+  });
 });
