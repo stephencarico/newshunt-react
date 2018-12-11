de94ea53d9b871f180b1028d4c820550641cc8d1
Stephen Carico <stephen.r.carico@gmail.com>
2018-12-10 18:25:07 -0800

    renamed files to NewsHunt

---
+++ src/NewsHunt.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './materialize.min.css';
+
+class NewsHunt extends Component {
+  render() {
+    return (
+      <div>NewsHunt</div>
+    );
+  }
+}
+
+export default NewsHunt;
--- src/NewsHunt.test.js
+++ src/NewsHunt.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import NewsHunt from './NewsHunt';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<NewsHunt />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
