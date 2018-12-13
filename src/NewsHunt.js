dbeda19c3c03491e13452b4265713811d3d8a3f8
Stephen Carico <stephen.r.carico@gmail.com>
2018-12-13 14:55:47 -0800

    added navbar and footer components

--- src/NewsHunt.js
+++ src/NewsHunt.js
@@ -3,10 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import './materialize.min.css';
 
+function Navbar() {
+  return (<div class="navbar-fixed">
+      <nav class="nav-extended">
+        <div class="nav-wrapper">
+          <img class="brand-logo center" src="images/news-hunt-color.png" />
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        </div>
+        <div class="container nav-wrapper">
+          <div class="nav-content">
+            <span>
+              <ul class="tabs tabs-transparent">
+                <li class="tab"><a href="/#/">All</a></li>
+                <li class="tab"><a href="/#/">Hacker News</a></li>
+                <li class="tab"><a href="/#/">Reddit</a></li>
+                <li class="tab"><a href="/#/">Product Hunt</a></li>
+                <li class="tab"><a href="/#/">Github Trending</a></li>
+              </ul>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </div>)
+}
+
+function Footer() {
+  return (<footer class="container">
+    <p>Created by <a href="https://github.com/robdrosenberg" target="_blank">Robert Rosenberg</a> and <a href="https://github.com/stephencarico" target="_blank">Stephen Carico</a></p>
+  </footer>)
+}
+
 class NewsHunt extends Component {
   render() {
     return (
-      <div>NewsHunt</div>
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import NewsHunt from './NewsHunt';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<NewsHunt />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
