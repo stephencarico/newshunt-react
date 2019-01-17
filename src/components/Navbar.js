d5aa57f5d5ab05ef3efdfa260de0c50fcb3336a0
Stephen Carico <stephen.r.carico@gmail.com>
2019-01-16 18:19:30 -0800

    created navbar for screen sizes 992px and less

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,15 @@ const Navbar = () => (
     <div className="nav-wrapper container">
       <img className="brand-logo" src="images/threadz-logo.png" alt="threadz-logo"></img>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <FilterTab filter={VisibilityFilters.LOAD_ALL}>ALL</FilterTab>
+        <FilterTab filter={VisibilityFilters.LOAD_HACKER_NEWS}>HACKER NEWS</FilterTab>
+        <FilterTab filter={VisibilityFilters.LOAD_REDDIT}>REDDIT</FilterTab>
+        <FilterTab filter={VisibilityFilters.LOAD_PRODUCT_HUNT}>PRODUCT HUNT</FilterTab>
+        <FilterTab filter={VisibilityFilters.LOAD_GITHUB_TRENDING}>GITHUB TRENDING</FilterTab>
+      </ul>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent">
         <FilterTab filter={VisibilityFilters.LOAD_ALL}>All</FilterTab>
         <FilterTab filter={VisibilityFilters.LOAD_HACKER_NEWS}>Hacker News</FilterTab>
         <FilterTab filter={VisibilityFilters.LOAD_REDDIT}>Reddit</FilterTab>
